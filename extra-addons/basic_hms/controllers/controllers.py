a7ba29e9aff2635b82bbec83fd5df07cd7fc6005
陳澔恩_Ubuntu <hank20011224@gmail.com>
2023-11-01 17:34:06 +0800

    up to Demo version

--- extra-addons/basic_hms/controllers/controllers.py
+++ extra-addons/basic_hms/controllers/controllers.py
@@ -1,25 +1,21 @@
 from odoo import http
+from odoo.http import request
 
 class YourController(http.Controller):
-    @http.route('/post', type='http', auth='public')
+    @http.route('/appointment', type='http', auth='public')
     def your_method(self, **kwargs):
         # 解析kwargs並提取需要的參數
         name = kwargs.get('name')
         status = kwargs.get('status')
-        description = kwargs.get('description')
         start = kwargs.get('start')
-        end = kwargs.get('end')
-        patient_id = kwargs.get('patient_id')
-        doctor_id = kwargs.get('doctor_id')
+        patient_name = kwargs.get('patient_name')
+        doctor_name = kwargs.get('doctor_name')
         # 建立新的Register記錄並將數據填入
-        # sudo -> super user
         register = http.request.env['medical.register'].sudo().create({
             'name': name,
             'status': status,
-            'description': description,
             'start': start,
-            'end': end,
-            'patient_id': patient_id,
-            'doctor_id': doctor_id
+            'patient_name': patient_name,
+            'doctor_name': doctor_name
         })
-        return "預約成功"
+        return "<h1>預約成功</h1>"
